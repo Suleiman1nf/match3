f7f90f86df5d640690a5b41d9d2fd9c6b4d6f703
suleiman1nf <whiletruez@gmail.com>
2024-04-06 21:57:23 +0300

    Randomize idle animation, fix sorting layers

---
+++ Assets/!Project/Scripts/Gameplay/Cube/CubeAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace _Project.Scripts.Gameplay.Cube
+{
+    [RequireComponent(typeof(Animator))]
+    public class CubeAnimation : MonoBehaviour
+    {
+        private static readonly int DestroyParam = Animator.StringToHash("Destroy");
+        private static readonly int Idle = Animator.StringToHash("Idle");
+        private const float IdleDelayMaxTime = 0.5f;
+        private Animator _animator;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            DelayIdle(this.GetCancellationTokenOnDestroy()).Forget();
+        }
+
+        public void PlayIdle()
+        {
+            _animator.SetTrigger(Idle);
+        }
+
+        public void PlayDeath()
+        {
+            _animator.SetTrigger(DestroyParam);
+        }
+
+        private async UniTaskVoid DelayIdle(CancellationToken cancellationToken)
+        {
+            await UniTask.WaitForSeconds(Random.Range(0, IdleDelayMaxTime), cancellationToken: cancellationToken);
+            PlayIdle();
+        }
+    }
+}
