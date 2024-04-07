b5d0e1759614bd8411db29b85643edc3fa359a5c
suleiman1nf <whiletruez@gmail.com>
2024-04-07 12:08:37 +0300

    add serializablegrid

---
+++ Assets/!Project/Scripts/Utils/GridUtils/ArrayElement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _Project.Scripts.Utils.GridUtils
+{
+    [Serializable]
+    public class ArrayElement<T>
+    {
+        public int Index0;
+        public int Index1;
+        public T Element;
+        public ArrayElement(int idx0, int idx1, T element)
+        {
+            Index0 = idx0;
+            Index1 = idx1;
+            Element = element;
+        }
+    }
+}
